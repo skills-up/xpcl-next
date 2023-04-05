c74e0474154bf77743aaec13c5f44ddd4bb2a9cc
evildeedz <sahilverma1311@protonmail.com>
2023-04-05 14:23:49 +0530

    Data table + pagination added

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
   createItem,
   deleteItem,
@@ -5,8 +6,19 @@ import {
   getList,
   updateItem,
 } from '../../api/xplorzApi';
+import Datatable from '../../components/datatable/Datatable';
 
 export default function Test() {
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    let tempArr = [];
+    for (let i = 0; i < 200; i++) {
+      tempArr.push({ id: i, desc: 'I am ' + i, timestamp: Date.now() });
+    }
+    setArr(tempArr);
+  }, []);
+
   const test = async () => {
     // Get List
     let response = await getList('roles', { paginate: 20, test: 1 });
@@ -28,9 +40,36 @@ export default function Test() {
     // response = await deleteItem('roles', 2);
     // console.log('deleteItem', response);
   };
+
+  const columns = [
+    {
+      Header: 'ID',
+      accessor: 'id',
+    },
+    {
+      Header: 'Description',
+      accessor: 'desc',
+    },
+    {
+      Header: 'Timestamp',
+      accessor: 'timestamp',
+    },
+    {
+      Header: 'Actions',
+      sortable: false,
+      // cell: () => <Button variant="danger" data-tag="allowRowEvents" data-action="delete"><FontAwesomeIcon icon={faTrash} /></Button>,
+      Cell: (data) => {
+        return <div className='flex flex-start'></div>;
+      },
+    },
+  ];
+
   return (
     <div>
       <button onClick={test}>Test</button>
+      <div className='mx-5'>
+        <Datatable columns={columns} data={arr} />
+      </div>
     </div>
   );
 }
