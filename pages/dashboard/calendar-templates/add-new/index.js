fcdd1bf9ccc528574b4afd9ee01feade7685265e
evildeedz <sahilverma1311@protonmail.com>
2023-04-11 13:38:01 +0530

    Calendar Template CRUD finished

---
+++ pages/dashboard/calendar-templates/add-new/index.js
@@ -0,0 +1,125 @@
+import Seo from '../../../../components/common/Seo';
+import Footer from '../../../../components/footer/dashboard-footer';
+import Header from '../../../../components/header/dashboard-header';
+import Sidebar from '../../../../components/sidebars/dashboard-sidebars';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { sendToast } from '../../../../utils/toastify';
+import { useEffect, useState } from 'react';
+import { createItem, getList } from '../../../../api/xplorzApi';
+import ReactSwitch from 'react-switch';
+import Select from 'react-select';
+
+const AddCalenderTemplate = () => {
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
+
+  const token = useSelector((state) => state.auth.value.token);
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // Checking if account id is not null
+    const response = await createItem('/calendar-templates', {
+      name,
+      content,
+    });
+    if (response?.success) {
+      sendToast('success', 'Created Calendar Template Successfully.', 4000);
+      router.push('/dashboard/calendar-templates');
+    } else {
+      sendToast(
+        'error',
+        response.data?.message ||
+          response.data?.error ||
+          'Failed to Create Calendar Template.',
+        4000
+      );
+    }
+  };
+
+  return (
+    <>
+      <Seo pageTitle='Add New Calendar Template' />
+      {/* End Page Title */}
+
+      <div className='header-margin'></div>
+
+      <Header />
+      {/* End dashboard-header */}
+
+      <div className='dashboard'>
+        <div className='dashboard__sidebar bg-white scroll-bar-1'>
+          <Sidebar />
+          {/* End sidebar */}
+        </div>
+        {/* End dashboard__sidebar */}
+
+        <div className='dashboard__main'>
+          <div className='dashboard__content d-flex flex-column justify-between bg-light-2'>
+            <div>
+              <div className='row y-gap-20 justify-between items-end pb-60 lg:pb-40 md:pb-32'>
+                <div className='col-12'>
+                  <h1 className='text-30 lh-14 fw-600'>Add New Calendar Template</h1>
+                  <div className='text-15 text-light-1'>Create a new calendar template.</div>
+                </div>
+                {/* End .col-12 */}
+              </div>
+              {/* End .row */}
+
+              <div className='py-30 px-30 rounded-4 bg-white shadow-3'>
+                <div>
+                  <form onSubmit={onSubmit} className='row col-12 y-gap-20'>
+                    <div className='col-12'>
+                      <div className='form-input'>
+                        <input
+                          onChange={(e) => setName(e.target.value)}
+                          value={name}
+                          placeholder=' '
+                          type='text'
+                          required
+                        />
+                        <label className='lh-1 text-16 text-light-1'>
+                          Name<span className='text-danger'>*</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className='col-12'>
+                      <div className='form-input'>
+                        <input
+                          onChange={(e) => setContent(e.target.value)}
+                          value={content}
+                          placeholder=' '
+                          type='text'
+                          required
+                        />
+                        <label className='lh-1 text-16 text-light-1'>
+                          Content<span className='text-danger'>*</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className='d-inline-block'>
+                      <button
+                        type='submit'
+                        className='button h-50 px-24 -dark-1 bg-blue-1 text-white'
+                      >
+                        Add Calendar Template
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <Footer />
+          </div>
+          {/* End .dashboard__content */}
+        </div>
+        {/* End dashbaord content */}
+      </div>
+      {/* End dashbaord content */}
+    </>
+  );
+};
+
+export default AddCalenderTemplate;
---
+++ pages/dashboard/calendar-templates/view/[view].js
@@ -0,0 +1,142 @@
+import Seo from '../../../../components/common/Seo';
+import Footer from '../../../../components/footer/dashboard-footer';
+import Header from '../../../../components/header/dashboard-header';
+import Sidebar from '../../../../components/sidebars/dashboard-sidebars';
+import ConfirmationModal from '../../../../components/confirm-modal';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { sendToast } from '../../../../utils/toastify';
+import { useEffect, useState } from 'react';
+import { deleteItem, getItem } from '../../../../api/xplorzApi';
+import ViewTable from '../../../../components/view-table';
+import CalendarTemplates from '../components/CalendarTemplates';
+
+const ViewCalendarTemplate = () => {
+  const [calendarTemplate, setCalendarTemplate] = useState([]);
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [idToDelete, setIdToDelete] = useState(-1);
+
+  const token = useSelector((state) => state.auth.value.token);
+  const router = useRouter();
+  useEffect(() => {
+    // Getting particular template
+    getCalendarTemplate();
+  }, [router.isReady]);
+
+  const getCalendarTemplate = async () => {
+    if (router.query.view) {
+      const response = await getItem('calendar-templates', router.query.view);
+      if (response?.success) {
+        let data = response.data;
+        // Converting time columns
+        if (data.created_at) {
+          data.created_at = new Date(data.created_at).toLocaleString('en-IN', {
+            dateStyle: 'medium',
+            timeStyle: 'short',
+          });
+        }
+        if (data.updated_at) {
+          data.updated_at = new Date(data.updated_at).toLocaleString('en-IN', {
+            dateStyle: 'medium',
+            timeStyle: 'short',
+          });
+        }
+        setCalendarTemplate(data);
+      } else {
+        sendToast(
+          'error',
+          response.data?.message ||
+            response.data?.error ||
+            'Could Not Fetch The Requested Calendar Template.'
+        );
+        router.push('/dashboard/calendar-templates');
+      }
+    }
+  };
+
+  const onCancel = async () => {
+    setConfirmDelete(false);
+    setIdToDelete(-1);
+  };
+  const onSubmit = async () => {
+    const response = await deleteItem('calendar-templates', idToDelete);
+    if (response?.success) {
+      sendToast('success', 'Deleted successfully', 4000);
+      router.push('/dashboard/calendar-templates');
+    } else {
+      sendToast(
+        'error',
+        response.data?.message ||
+          response.data?.error ||
+          'Unexpected Error Occurred While Trying to Delete this Calendar Template',
+        4000
+      );
+    }
+    onCancel();
+  };
+
+  return (
+    <>
+      <Seo pageTitle='View Calendar Template' />
+      {/* End Page Title */}
+
+      <div className='header-margin'></div>
+
+      <Header />
+      {/* End dashboard-header */}
+
+      <div className='dashboard'>
+        <div className='dashboard__sidebar bg-white scroll-bar-1'>
+          <Sidebar />
+          {/* End sidebar */}
+        </div>
+        {/* End dashboard__sidebar */}
+
+        <div className='dashboard__main'>
+          <div className='dashboard__content d-flex flex-column justify-between bg-light-2'>
+            <div>
+              <div className='row y-gap-20 justify-between items-end pb-60 lg:pb-40 md:pb-32'>
+                <div className='col-12'>
+                  <h1 className='text-30 lh-14 fw-600'>View Calendar Template</h1>
+                  <div className='text-15 text-light-1'>
+                    Get extended details of a calendar template.
+                  </div>
+                </div>
+                {/* End .col-12 */}
+              </div>
+              {/* End .row */}
+
+              <div className='py-30 px-30 rounded-4 bg-white shadow-3'>
+                {confirmDelete && (
+                  <ConfirmationModal
+                    onCancel={onCancel}
+                    onSubmit={onSubmit}
+                    title='Do you really want to delete this calendar template?'
+                    content='This will permanently delete the calendar template. Press OK to confirm.'
+                  />
+                )}
+                <ViewTable
+                  data={calendarTemplate}
+                  onEdit={() =>
+                    router.push('/dashboard/calendar-templates/edit/' + router.query.view)
+                  }
+                  onDelete={() => {
+                    setIdToDelete(router.query.view);
+                    setConfirmDelete(true);
+                  }}
+                />
+              </div>
+            </div>
+
+            <Footer />
+          </div>
+          {/* End .dashboard__content */}
+        </div>
+        {/* End dashbaord content */}
+      </div>
+      {/* End dashbaord content */}
+    </>
+  );
+};
+
+export default ViewCalendarTemplate;
