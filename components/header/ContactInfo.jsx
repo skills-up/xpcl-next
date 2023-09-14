83808147279825773ec9a41117629520e8081d86
evildeedz <sahilverma1311@protonmail.com>
2023-09-15 02:30:31 +0530

    Mobile sidebar

--- components/header/ContactInfo.jsx
+++ components/header/ContactInfo.jsx
@@ -2,23 +2,23 @@ const ContactInfo = () => {
   const contactContent = [
     {
       id: 1,
-      title: "Toll Free Customer Care",
-      action: "tel:+(1) 123 456 7890",
-      text: "+(1) 123 456 7890",
+      title: 'Phone',
+      action: 'tel:+912266121000',
+      text: '+91 22 66121000',
     },
     {
       id: 2,
-      title: "Need live support?",
-      action: "mailto:xyz@abc.com",
-      text: "hi@gotrip.com",
+      title: 'Need live support?',
+      action: 'mailto:support@xplorz.com',
+      text: 'support[at]xplorz.com',
     },
   ];
   return (
     <>
       {contactContent.map((item) => (
-        <div className="mb-20" key={item.id}>
-          <div className={"text-14"}>{item.title}</div>
-          <a href={item.action} className="text-18 fw-500 text-dark-1 mt-5">
+        <div className='mb-20' key={item.id}>
+          <div className={'text-14'}>{item.title}</div>
+          <a href={item.action} className='text-18 fw-500 text-dark-1 mt-5'>
             {item.text}
           </a>
         </div>
